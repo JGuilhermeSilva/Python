1e6837af7092d97a6c31b0a5f0ec179a95cd6c8c
JGuilhermeSilva <jguilherme1323@gmail.com>
2026-05-10 11:08:56 -0300

    implementei . bind

--- adicionarNome.py
+++ adicionarNome.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-def adicionar_nome():
+def adicionar_nome(event = None):
     nome = campo_nome.get()
     if nome != "":
         lista.insert(tk.END, f"Presente: {nome}")
@@ -9,17 +9,24 @@ def adicionar_nome():
 janela = tk.Tk()
 janela.title("Chamada de turma")
 #janela.geometry("500x500")
-
+#
+janela.minsize(300, 300)
+janela.maxsize(500, 500)
+janela.resizable(True, False)
+#
 rotulo = tk.Label(janela, text="Digite o nome do aluno")
 rotulo.pack()
 
 campo_nome = tk.Entry(janela) #adicionar janela
 campo_nome.pack()
 
-botao = tk.Button(janela, text="Adicionar", command=adicionar_nome)
+janela.bind('<Return>', adicionar_nome)
+botao = tk.Button(janela,event=None, text="Adicionar", command=adicionar_nome)
 botao.pack()
+
 
 lista = tk.Listbox(janela)
 lista.pack()
+
 
 janela.mainloop()
