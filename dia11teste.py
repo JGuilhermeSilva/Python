885d7f6c7aeac3d99b108b2b78bd42d1a3bdb981
JGuilhermeSilva <jguilherme1323@gmail.com>
2026-05-11 20:10:03 -0300

    teste final dia 11 2

--- dia11teste.py
+++ dia11teste.py
@@ -1,9 +1,17 @@
-compras = ['Arroz', 'Mortandela', 'Macarrao', 'Farinha', 'Cuscuz', 'Feijao', 'Ovo']
-compras.insert(1, 'tapioca')
+compras = ['Arroz', 'Mortandela', 'Macarrao', 'Farinha', 'Cuscuz', 'Feijao', 'Ovo', 'Feijao']
+#compras.insert(1, 'tapioca')
 
 '''compras2 = ['maca', 'goiaba', 'banana', 'melancia', 'kiwi', 'acerola', 'mamao']
 compras.extend(compras2)
 '''
-compras.remove("Mortandela")
-compras.clear()
+#compras.remove("Mortandela")
+#compras.clear() limpa toda a lista
 print(compras)
+
+#compras.sort(reverse=True)
+#print(compras)
+
+
+
+'''for c, v in enumerate(compras):
+    print(f"encontrei na posicao {c} o valor de {v}")'''
