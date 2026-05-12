813199241b8ba83464ff28d2da3b19dd9a51574d
JGuilhermeSilva <jguilherme1323@gmail.com>
2026-05-12 20:02:17 -0300

    dia 12

--- miniProjetoCompras.py
+++ miniProjetoCompras.py
@@ -1,10 +1,8 @@
 '''programa que calcula valor de uma lista de compras
-de supermercado
-
-'''
+de supermercado'''
 
 import tkinter as tk
-#from tkinter import ttk
+from tkinter import ttk
 
 #lista para armazenar os produtos
 compras = []
@@ -26,52 +24,77 @@ def adicionarProduto():
         campo_nome.delete(0, tk.END)
         campo_quantidade.delete(0, tk.END)
         campo_valor.delete(0, tk.END)
+        #integrando a função de calcular valor para atualizar
+        #automaticamente ao inserir produto na lista
+        calcularTotal()
 
 def calcularTotal():
     total = 0
     for produto in compras:
         total += produto["quantidade"] * produto["valor"]
-        resultado.config(text=f"Valor Total da compra: R${total:.2f}")
+        somaTotal.config(text=f"Valor total da compra: R${total:.2f}")
 
 def forma_pagamento():
     pagamento = campo_pagamento.get()
     if pagamento.lower() == 'cartao':
-        resultado.config(text="Pagamento escolhido: Cartão")
+        escolhaFormaPagamento.config(text="Pagamento escolhido: Cartão", bg='green')
     elif pagamento.lower() == 'pix':
-        resultado.config(text="Pagamento escolhido: Pix")
+        escolhaFormaPagamento.config(text="Pagamento escolhido: Pix", bg='green')
     elif pagamento.lower() == 'especie':
-        resultado.config(text="Pagamento escolhido: Em espécie")
+        escolhaFormaPagamento.config(text="Pagamento escolhido: Em espécie", bg='green')
     else:
-        resultado.config(text="Escolha uma forma de pagamento válida!!")
+        escolhaFormaPagamento.config(text="Escolha uma forma de pagamento válida!!", bg='red')
 
-
+#iniciando interface grafica
 janela = tk.Tk()
 janela.title("Projeto lista de compras")
-janela.geometry("500x500")
+janela.geometry("600x800")
+janela.config(bg="grey")
 
-rotulo = tk.Label(janela, text="Lista de compras")
+
+rotulo = tk.Label(janela, text="Lista de compras", font=("Arial" ,13, "bold"))
 rotulo.pack()
 
+rotulo_nome = tk.Label(janela, text="Nome do produto:").pack()
 campo_nome = tk.Entry(janela, text="")
 campo_nome.pack()
 
+rotulo_quantidade = tk.Label(janela, text="Quantidade do produto:").pack()
 campo_quantidade = tk.Entry(janela, text="")
 campo_quantidade.pack()
 
+rotulo_valor = tk.Label(janela, text="Valor do produto:").pack()
 campo_valor = tk.Entry(janela, text="")
 campo_valor.pack()
 
-janela.bind('<Return>', adicionarProduto)
-botao = tk.Button(janela, text="Adicionar", command=adicionarProduto)
+#janela.bind('<Return>', adicionarProduto)
+botao = ttk.Button(janela, text="Adicionar", command=adicionarProduto)
 botao.pack()
 
+#Lista visual para mostrar produtos inseridos
 lista = tk.Listbox(janela)
 lista.pack()
 
-botao2 = tk.Button(janela, text="Mostrar valor", command=calcularTotal)
-botao2.pack()
+#criando botao para mostrar valor total da compra
+#somaTotal = tk.Button(janela, text='Calcular total:', command=calcularTotal)
+#somaTotal.pack()
 
-resultado = tk.Label(janela, text="")
-resultado.pack()
+#mostra valor total da compra
+somaTotal = tk.Label(janela, text="", foreground="red")
+somaTotal.pack()
+
+tk.Label(janela, text="Digite uma forma de pagamento('Pix', 'Cartão', ou 'Em espécie')").pack()
+
+#recebe uma entrada da forma de pagamento
+campo_pagamento = tk.Entry(janela, text='')
+campo_pagamento.pack()
+
+#adiciona a forma de pagamento da compra
+botao_pagamento = ttk.Button(janela, text="Adicionar Forma", command=forma_pagamento)
+botao_pagamento.pack()
+
+#retorna ao usuario se a forma de pagamento é valida
+escolhaFormaPagamento = tk.Label(janela, text="", foreground="black")
+escolhaFormaPagamento.pack()
 
 janela.mainloop()
