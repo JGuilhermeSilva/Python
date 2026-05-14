9b4d509dee5b9275cc8d2540028e2fcf981680ab
JGuilhermeSilva <jguilherme1323@gmail.com>
2026-05-13 22:28:39 -0300

    funcao calculadora com tkinter finalizada

--- funcaoCalculadoracomTkinter.py
+++ funcaoCalculadoracomTkinter.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 
 
-def calculadora():
+def calculadora(event=None):
+    num1 = float(entrada1.get())
+    num2 = float(entrada2.get())
+    escolha = escolhaOP.get()
     if (escolha=="soma" or escolha=="+"):
         soma = num1 + num2
-        resultado.config(text=f"Soma de {num1}+{num2} = {soma}")
+        resultado.config(text=f"Soma de {num1}+{num2} = {soma:.2f}")
 
     elif (escolha=="subtracao" or escolha=="-"):
         subtracao = num1-num2
-        resultado.config(text=f"Subtracao de {num1}-{num2} = {subtracao}")
+        resultado.config(text=f"Subtracao de {num1}-{num2} = {subtracao:.2f}")
 
     elif (escolha=="multiplicacao" or escolha=="*"):
         multiplicacao = num1 * num2
-        resultado.config(text=f"Multiplicacao de {num1}x{num2} = {multiplicacao}")
+        resultado.config(text=f"Multiplicacao de {num1}x{num2} = {multiplicacao:.2f}")
 
     elif (escolha=="divisao" or escolha=="/"):
         if num2 != 0:
             divisao = num1/num2
-            resultado.config(text=f"Divisão de {num1}/{num2} = {divisao}\n")
+            resultado.config(text=f"Divisão de {num1}/{num2} = {divisao:.2f}\n")
         else:
             resultado.config(text=f"ERRO: Não é possivel dividir por 0!!!")
     
@@ -31,17 +34,18 @@ janela.geometry("500x500")
 tk.Label(janela, text="Calculadora Python/Tkinter").pack()
 
 tk.Label(janela, text="Inserir valor 1:").pack()
-num1 = tk.Entry(janela)
-num1.pack()
+entrada1 = tk.Entry(janela, text="")
+entrada1.pack()
 
 tk.Label(janela, text="Inserir valor 2:").pack()
-num2 = tk.Entry(janela)
-num2.pack()
+entrada2 = tk.Entry(janela, text="")
+entrada2.pack()
 
-rotuloEscolha = tk.Label(janela, text="Insira o tipo de operação:")
-rotuloEscolha.pack()
-escolha = tk.Entry(janela, text="")
-escolha.pack()
+tk.Label(janela, text="Insira o tipo de operação:").pack()
+escolhaOP = tk.Entry(janela, text="")
+janela.bind('<Return>', calculadora)
+escolhaOP.pack()
+
 
 botao = tk.Button(janela, text="Calcular", command=calculadora)
 botao.pack()
